800f42b3dedfa86768f8aba0b4995255c899aa2d
Jordan Kline <jordan.kline@gmail.com>
2020-01-16 09:37:28 -0800

    added body-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import router from './routes/routes';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', router);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
