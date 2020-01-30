b552335599ff24f0073159e8ab0890abe11308de
Jordan Kline <jordan.kline@gmail.com>
2020-01-29 23:08:11 -0800

    added key to work with sequelize index model file

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,13 +4,16 @@ module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
+    use_env_variable: true,
   },
   test: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
+    use_env_variable: true,
   },
   production: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
+    use_env_variable: true,
   },
 };
